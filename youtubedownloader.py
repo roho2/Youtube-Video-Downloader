cf993634c8c33c5162dfc94f4b8b753b0ff4596c
RJ Hollinger <33728865+roho2@users.noreply.github.com>
2020-11-08 00:12:06 -0500

    Adding initial file
    Adding initial file. Just sets up the main function, no inner workings... yet!

---
+++ youtubedownloader.py
@@ -0,0 +1,18 @@
+#  _______________________________________
+# |										  |
+# |	       Youtube Video Downloader       |
+# |			      Created by: 	     	  |
+# |	  Robert Hollinger & Robinson Nunez   |                  
+# |				 Version 0.0.0			  |
+#  ---------------------------------------
+
+
+
+
+def main():
+    # Placeholder comment
+
+
+if __name__ == '__main__':
+    main()
+    
